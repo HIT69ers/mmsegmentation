aa01620b3ecd1b8c8ee145f269061a9e85dfc7c9
HIT69ers <2022113442@stu.hit.edu.cn>
2026-03-10 20:27:04 +0800

    lmlpdecoder-segformer

---
+++ configs/segformer/mysegformer_05_mit-b3_ld_1xb8-50k_nyuv2-480x640.py
@@ -0,0 +1,11 @@
+_base_ = ['./mysegformer_10_mit-b0_ld_1xb8-50k_nyuv2-480x640.py']
+
+checkpoint = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segformer/mit_b3_20220624-13b1141c.pth'  # noqa
+
+model = dict(
+    backbone=dict(
+        init_cfg=dict(type='Pretrained', checkpoint=checkpoint),
+        embed_dims=64,
+        num_layers=[3, 4, 18, 3],
+        downsample_ratio=0.5),
+    decode_head=dict(in_channels=[128, 320, 512]))
